f6f93717b976212eb6161f2245930c08f14473bf
rohkeaID <arseniigerasimov@gmail.com>
2018-11-14 18:14:38 +0500

    OpenMP parallelization for UNK-files: fixed nspinor

--- src/genw90unk.f90
+++ src/genw90unk.f90
@@ -10,7 +10,6 @@ subroutine genw90unk
 ! !USES:
 use modmain
 use modw90
-use modw90overlap
 use modmpi
 use modomp
 
@@ -25,7 +24,10 @@ integer i,j,is,m
 integer nrc,nrci
 integer nthd
 integer fileID
+integer redkfil,nstsv_,recl
 real(8) vgqc(3),gqc
+logical exists
+real(8) t1
 ! allocatable arrays
 complex(8), allocatable :: wfmt(:,:,:,:),wfir(:,:,:)
 complex(8), allocatable :: zwfmt(:,:,:,:)
@@ -34,11 +36,13 @@ complex(8), allocatable :: ylmgq(:),sfacgq(:)
 complex(8), allocatable :: expmt(:,:)
 integer,    allocatable :: igpig(:,:)
 integer,    allocatable :: ngp(:)
+real(8),    allocatable :: evalsv_(:)
 ! automatic arrays
 character(8)            :: fmt_ikp,fmt_is ! format descriptor
 real(8)                 :: bqc(3)
 character(8)            :: ikp_str,is_str
 character(256)          :: filename
+real(8)                 :: vkl_(3)
 
 reducek0=reducek ! if reducek=1 was used in ground state calculations,
                  ! need to regenerate the eigenvectors set for the full BZ.
@@ -58,6 +62,41 @@ call linengy
 call genapwfr
 ! generate the local-orbital radial functions
 call genlofr
+
+! check that EVECSV.OUT has all necessary k-points
+allocate(evalsv_(nstsv))
+redkfil=0
+inquire(iolength=recl) vkl_,nstsv_,evalsv_
+do ikp=1,nkpt
+  exists=.false.
+  t1=9.d99
+  inquire(file='EVALSV'//trim(filext),exist=exists)
+  if(exists) then
+    open(70,file='EVALSV'//trim(filext),action='READ',form='UNFORMATTED', &
+        access='DIRECT',recl=recl,err=101)
+    read(70,rec=ikp,err=101) vkl_,nstsv_,evalsv_
+    close(70)
+    t1=abs(vkl(1,ikp)-vkl_(1))+abs(vkl(2,ikp)-vkl_(2))+abs(vkl(3,ikp)-vkl_(3))
+  end if
+101 continue
+  if (.not.exists.or.t1.gt.epslat.or.nstsv.ne.nstsv_) then
+    redkfil=1
+    exit
+  end if
+end do
+! If kpoint not found in saved eigen-values/vectors, then need to recompute
+! EVEC*OUT.
+if (redkfil.ne.0) then
+! compute the overlap radial integrals
+  call olprad
+! compute the Hamiltonian radial integrals
+  call hmlrad
+! generate the spin-orbit coupling radial functions
+  call gensocfr
+! generate the first- and second-variational eigenvectors and eigenvalues
+  call genevfsv
+end if
+
 
 fmt_ikp = '(I5.5)' ! format of k-points in UNK filename an integer of width 5 with zeros at the left
 fmt_is = '(I1.1)'
@@ -93,9 +132,9 @@ do ikp=1,nkpt
 
   call genwfsvp(.false.,.false.,wann_nband,wann_bands,ngridg,igfft,vkl(:,ikp),ngp,igpig,wfmt,ngtot,wfir) 
 
-  ! b-vector in Cartesian coordinates
+  ! k-vector in Cartesian coordinates
   call r3mv(bvec,vkl(:,ikp),bqc)
-  ! generate the phase factor function exp(ib.r) in the muffin-tins
+  ! generate the phase factor function exp(ik.r) in the muffin-tins
   call gengqrf(bqc,vgqc,gqc,jlgqr,ylmgq,sfacgq)
   call genexpmt(1,jlgqr,ylmgq,1,sfacgq,expmt)
 
@@ -119,11 +158,12 @@ do ikp=1,nkpt
     end do
   end do
 
+  fileID = 600+(ikp-1)*nspinor
   do is = 1,nspinor
     write(ikp_str,fmt_ikp) ikp ! converting integer to string using an 'internal file'
     write(is_str,fmt_is) is ! converting integer to string using an 'internal file'
     filename = 'UNK'//trim(ikp_str)//'.'//trim(is_str)
-    fileID=600+ikp+is
+    fileID=fileID+is-1
     open(fileID,file=filename,action='WRITE',form='FORMATTED')
     
     write(fileID,*) np3d(1),np3d(2),np3d(3),ikp,wann_nband
@@ -132,16 +172,16 @@ do ikp=1,nkpt
     end do
 
     close(fileID)
-  !$OMP CRITICAL(genw90unk_)
-    if (mp_mpi) then
-      write(*,'("  Info(Wannier Unk): completed ",I6," of ",I6," k-points")')&
-                                                                     &ikp,nkpt
-    end if
-  !$OMP END CRITICAL(genw90unk_)
-
   end do
 
-end do
+!$OMP CRITICAL(genw90unk_)
+  if (mp_mpi) then
+    write(*,'("  Info(Wannier Unk): completed ",I6," of ",I6," k-points")')&
+                                                                   &ikp,nkpt
+  end if
+!$OMP END CRITICAL(genw90unk_)
+
+end do!End loop over k points
 !$OMP END DO
 
 deallocate(ngp)
